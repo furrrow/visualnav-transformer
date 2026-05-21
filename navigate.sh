10730fcf2fee5d663e2ad73e57c3fe7d790c2054
Gershom Seneviratne <gershom.seneviratne@gmail.com>
2026-05-21 17:02:29 -0400

    navigate.sh

--- navigate.sh
+++ navigate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export PYTHONPATH=/opt/ros/humble/lib/python3.10/site-packages:/opt/ros/humble/local/lib/python3.10/dist-packages
+export PYTHONPATH=/workspace/prune/policies/visualnav-transformer:$PYTHONPATH
 
 # Create a new tmux session
 session_name="visualnav_$(date +%s)"
@@ -14,17 +16,15 @@ tmux splitw -h -p 50 # split it into two halves
 
 # Run the navigate.py script with command line args in the second pane
 tmux select-pane -t 0
-tmux send-keys "source /workspace/prune/setup.bash" Enter
-#tmux send-keys "source .venv/bin/activate" Enter
-#tmux send-keys "python navigate.py $@"
+# tmux send-keys "export PYTHONPATH=/workspace/prune/policies/flownav" Enter
+# tmux send-keys "source /workspace/prune/setup.bash" Enter
+# tmux send-keys "source /opt/ros/humble/setup.bash" Enter
 tmux send-keys "uv run deployment/src/navigate.py --dir antonov"
 
 
 # Run the pd_controller.py script in the fourth pane
 tmux select-pane -t 1
-tmux send-keys "source /workspace/prune/setup.bash" Enter
-#tmux send-keys "source .venv/bin/activate" Enter
-#tmux send-keys "python ../pd_controller.py" Enter
+# tmux send-keys "source /workspace/prune/setup.bash" Enter
 tmux send-keys "uv run deployment/src/pd_controller.py"
 
 
